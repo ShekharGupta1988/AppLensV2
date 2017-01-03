47128e49df78e04de29a6c6ff3e7c25a12a2bebd
aspaan <askamboj@microsoft.com>
2017-01-03 11:25:25 -0800

    resource group fixed

--- AppLensV2/Helpers/SupportObserverClient.cs
+++ AppLensV2/Helpers/SupportObserverClient.cs
@@ -85,7 +85,7 @@ namespace AppLensV2
         /// <param name="subscription">Site Subscription</param>
         /// <param name="webspace">Site WebSpace</param>
         /// <returns>Stamp</returns>
-        internal static async Task<dynamic> GetResourceGroup(string subscription, string webspace)
+        internal static async Task<dynamic> GetResourceGroup(string site)
         {
             using (var client = new HttpClient())
             {
@@ -93,10 +93,9 @@ namespace AppLensV2
                 client.MaxResponseContentBufferSize = Int32.MaxValue;
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-                var serializedParameters = JsonConvert.SerializeObject(new Dictionary<string, string>() { { "subscriptionid", subscription }, { "webspacename", webspace } });
-                client.DefaultRequestHeaders.Add("client-hash", SignData(serializedParameters, SimpleHashAuthenticationHashKey));
-               
-                var response = await client.GetAsync(SupportObserverApiEndpoint + "subscriptionid/" + subscription + "/webspacename/" + webspace + "/resourcegroupname?api-version=2");
+                client.DefaultRequestHeaders.Add("client-hash", SignData(string.Format("{{\"site\":\"{0}\"}}", site), SimpleHashAuthenticationHashKey));
+
+                var response = await client.GetAsync(SupportObserverApiEndpoint + "sites/" + site + "/resourcegroupname?api-version=2");
 
                 if (response.IsSuccessStatusCode)
                 {
