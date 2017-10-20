493e63dfb6aac20a2f5c0a84a48a7d8e6fa6d2af
Hawk Foreste <hawfor@microsoft.com>
2017-10-20 15:48:35 -0700

    Saving progress

--- AppLensV2/app/Detector/DetectorViewDirective.ts
+++ AppLensV2/app/Detector/DetectorViewDirective.ts
@@ -20,16 +20,20 @@ module SupportCenter {
     }
 
     export class DetectorViewCtrl {
-        public static $inject: string[] = ["DetectorsService", "$stateParams", "$window", "FeedbackService", "$mdToast", "$timeout", "$scope"];
+        public static $inject: string[] = ["DetectorsService", "$stateParams", "$window", "FeedbackService", "$mdToast", "$timeout"];
         public showDetailedView: boolean = true;
         public detectorFeedbackOption: number = -1;
         private nameElement: any;
         private detectorName: string;
         public api: any;
         public isLoading: boolean;
-        public isSolutionsProvided: boolean = false;
 
-        constructor(private DetectorsService: IDetectorsService, private $stateParams: IStateParams, private $window: angular.IWindowService, private FeedbackService: IFeedbackService, private $mdToast: angular.material.IToastService, private $timeout: ng.ITimeoutService, private $scope: IDetectorViewScope) {
+        public abnormaltimeperiods: any;
+        public singleObservationMessage: string;
+        public singleSolutionMessage: string;
+        public metricsets: DiagnosticMetricSet[];
+
+        constructor(private DetectorsService: IDetectorsService, private $stateParams: IStateParams, private $window: angular.IWindowService, private FeedbackService: IFeedbackService, private $mdToast: angular.material.IToastService, private $timeout: ng.ITimeoutService) {
             this.isLoading = true;
             this.nameElement = document.getElementById('name');
             let self = this;
@@ -57,11 +61,6 @@ module SupportCenter {
             return false;
         }
 
-        isSolutionProvided(): boolean {
-            console.log(this.$scope.abnormaltimeperiods);
-            return false;
-        }
-
         sendDetectorFeedback(detectorName: string, feedbackOption: number) {
 
             if (this.detectorFeedbackOption === -1) {
@@ -76,6 +75,70 @@ module SupportCenter {
                 });
             }
         }
+
+        isSolutionProvided(): boolean {
+            if (angular.isDefined(this.abnormaltimeperiods) && this.abnormaltimeperiods.length !== 0) {
+                for (let abnormalTimePeriod of this.abnormaltimeperiods) {
+                    if (abnormalTimePeriod.Solutions.length > 0) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        isSameObservations(): boolean {
+            let observations: string[] = [];
+            if (angular.isDefined(this.abnormaltimeperiods) && this.abnormaltimeperiods.length !== 0) {
+                for (let abnormaltimeperiod of this.abnormaltimeperiods) {
+                    observations.push(abnormaltimeperiod.Message);
+                }
+            }
+
+            let s = observations.pop();
+
+            let isSame = observations.every((ss: string) => {
+                return s === ss;
+                }
+            )
+
+            if (isSame) {
+                this.singleObservationMessage = s;
+            }
+
+            let onlyOneObservationProvided = this.abnormaltimeperiods.length == 1;
+
+            return isSame && !onlyOneObservationProvided;
+        }
+
+        isSameSolution(): boolean {
+            let solutions: string[] = [];
+            if (angular.isDefined(this.abnormaltimeperiods) && this.abnormaltimeperiods.length !== 0) {
+                for (let abnormaltimeperiod of this.abnormaltimeperiods) {
+                    for (let solution of abnormaltimeperiod.Solutions) {
+                        if (solution.Description !== '') {
+                            solutions.push(solution.DisplayName + ": " + solution.Description);
+                        } else {
+                            solutions.push(solution.DisplayName);
+                        }
+                    }
+                }
+            }
+
+            let s = solutions.pop();
+
+            let isSame = solutions.every((ss: string) => {
+                return s === ss;
+            }
+            )
+
+            if (isSame) {
+                this.singleSolutionMessage = s;
+            }
+
+            return isSame;
+        }
+
     }
 
     export class DetectorViewDir implements ng.IDirective {
@@ -102,8 +165,7 @@ module SupportCenter {
             selectedworker: '=',
             detectorsource: '=',
             abnormaltimeperiod: '=',
-            abnormaltimeperiods: '=',
-
+            abnormaltimeperiods: '='
         };
     }
 }
